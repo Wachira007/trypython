d68fc532b8bfb0a01e0b8232370416ca50af1592
Wachira <dannielmutua00@gmail.com>
2024-12-07 15:52:49 +0300

    using pandas

---
+++ import pnd.py
@@ -0,0 +1,12 @@
+import pandas as pd
+#saving the file path to a variable
+n_file_path = 'd://train.csv'
+
+#reading the file and storing it in a DATAFRAME
+home_data = pd.read_csv(n_file_path)
+
+#describe the data
+home_data.describe()
+
+#calculate the mean of lot size
+lot_size = home_data['LotArea'].mean()
